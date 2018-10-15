a028c6d3d1624886c3a3f097a31c26468ba969ec
mostafa <mostafasamer1999@hotmail.com>
2018-10-15 21:47:35 +0200

    Person

--- src/miniSchool/Person.java
+++ src/miniSchool/Person.java
@@ -3,5 +3,21 @@ package miniSchool;
 public class Person {
 
 	public String name;
+	public String Email;
+	private int SSN;
+	
+	
+	
+	public int getSSN() {
+		
+		return SSN;
+		
+	}
+	
+	public void setSSN(int sSN) {
+		
+		SSN = sSN;
+		
+	}
 	
 }
