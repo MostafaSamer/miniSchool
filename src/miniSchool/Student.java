cf39ba76ff0cf29b9bbe87fbb314458bd7559ce2
mostafa <mostafasamer1999@hotmail.com>
2018-10-15 22:42:20 +0200

    Not Complete

--- src/miniSchool/Student.java
+++ src/miniSchool/Student.java
@@ -3,5 +3,14 @@ package miniSchool;
 public class Student extends Person {
 	
 	private float GPA;
+
+	public float getGPA() {
+		return GPA;
+	}
+
+	public void setGPA(float gPA) {
+		GPA = gPA;
+	}
+	
 	
 }
